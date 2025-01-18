2d577cfc9424a65ceca0f880a642543c5304b827
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2025-01-17 23:21:18 -0300

    Subindo arquivos inicias do projeto

---
+++ src/common/infrastructure/env/index.ts
@@ -0,0 +1,29 @@
+import { AppError } from '@/common/domain/errors/app-error'
+import 'dotenv/config'
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'prodcution', 'test'])
+    .default('development'),
+
+  PORT: z.coerce.number().default(3333),
+  API_URL: z.string().default('http://localhost:3333'),
+  DB_TYPE: z.literal('postgres').default('postgres'),
+  DB_HOST: z.string().default('localhost'),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_SCHEMA: z.string().default('public'),
+  DB_NAME: z.string().default('postgres'),
+  DB_USER: z.string().default('postgres'),
+  DB_PASS: z.string().default('postgres'),
+  MY_SECRET: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success == false) {
+  throw new AppError('Invalid environment variables')
+}
+
+export const env = _env.data
