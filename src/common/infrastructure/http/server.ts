7106a5596beebf5912336d18a2432b34394bf9f7
Otavin96 <otaviolazzarotto@icloud.com>
2025-01-18 00:19:52 -0300

    Criando middleware de erro

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,14 +1,5 @@
-import express from 'express'
-import cors from 'cors'
 import { app } from './app'
 import { env } from '../env/'
-
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world!' })
-})
 
 app.listen(env.PORT | 3333, () => {
   console.log(`Server is running on port ${env.PORT}`)
